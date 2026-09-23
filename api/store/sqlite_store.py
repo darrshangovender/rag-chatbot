33d261648975f3022c5812db2db7443a7d22490f
Darrshan Govender <darrshangovender@gmail.com>
2026-09-23 07:36:11 +0200

    Update api/store/sqlite_store.py

--- api/store/sqlite_store.py
+++ api/store/sqlite_store.py
@@ -15,9 +15,9 @@ from __future__ import annotations
 
 import json
 import sqlite3
+from collections.abc import Iterable
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterable
 
 import numpy as np
 
